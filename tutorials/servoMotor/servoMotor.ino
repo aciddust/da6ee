3c1eaea825798cb271ea309bb83072f8c38f736a
안성철 <acidlab.help@gmail.com>
2017-04-19 11:31:06 +0900

    서보모터 구동 예제

---
+++ tutorials/servoMotor/servoMotor.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+const int servoPin = 9;
+
+Servo myservo;
+int pos = 0;
+
+void setup() {
+  myservo.attach(servoPin);
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
